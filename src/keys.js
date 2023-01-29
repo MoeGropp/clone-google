7f7e369ceeec6667cfb30afafdf8d45278c60695
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-29 16:01:07 -0600

    01/29 Restarting
    Restarting google project because something wasn't working right with NPM. So I started a fresh repository, c&p what I had in the original project and cont the tutorial

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyDmbN8T1u8ImHYRPnhIff9AWynG_n99eTQ";
+
+export default API_KEY;
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,22 @@
+import { SettingsSystemDaydreamTwoTone } from "@mui/icons-material";
+import { useState, useEffect } from "react";
+import API_KEY from "./keys";
+
+const CONTEXT_KEY = "d6a1ef4834dae4296";
+
+const useGoogleSearch = (term) => {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+const fetchData = async() => {
+    fetch(
+        `https://www.googleapis.com/customesearch/v1?keys=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+    )
+    .then(response => response.json())
+    .then(result => {setData(result)})
+}
+fetchData();
+    }, [term])
+return {data}
+};
+
+export default useGoogleSearch;
