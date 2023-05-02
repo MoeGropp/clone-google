11d0605f0f55ce924c09befdce1d3248e74815fd
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-05-02 10:45:11 -0500

    trying to fix stuff
    So it can be loaded onto Netlify

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -10,7 +10,7 @@ import { actionTypes } from "../reducer";
 
 
 function Search({hideButtons = false}) {
-    const [{}, dispatch ] = useStateValue();
+    const [{term}, dispatch ] = useStateValue();
     const[input, setInput] = useState("");
     const history = useNavigate();
     const search = (e) => {
